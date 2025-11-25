7321fe85d23f1eb6cded4e37a84515c728bb5d04
Alejandro Quicaño <alejandroxd25k@gmail.com>
2025-11-24 20:39:07 -0500

    imp: Update .env properties

--- db/scripts_help.sh
+++ db/scripts_help.sh
@@ -3,3 +3,6 @@ docker build -t auth_nerdery .
 
 # Run the image with a volume to persist the DB data
 docker run -d -p 5432:5432 -v pgdata:/var/lib --name auth_nerdery_container auth_nerdery
+
+# Conect to the DB using psql
+#psql -h localhost -p 5432 -U admin -d auth
---
+++ src/main/java/com/nerdery/auth/models/BranchModel.java
@@ -0,0 +1,5 @@
+package com.nerdery.auth.models;
+
+public class BranchModel {
+    
+}
